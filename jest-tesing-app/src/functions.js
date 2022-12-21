853635489bc00b2af18c41265caf74e2abca4693
Barkley <barkley.santo@gmail.com>
2022-12-21 16:32:39 +0800

    broken axios , use outside module

--- jest-tesing-app/src/functions.js
+++ jest-tesing-app/src/functions.js
@@ -1,6 +1,8 @@
+const axios = require("axios");
+
 const functions = {
   add: (num1, num2) => num1 + num2, // ES6 arrow function
-  subtract: function (num1, num2) {
+  subtract: function(num1, num2) {
     return num1 + num2
   }, // ES5 function declaration
   isNull: () => null,
@@ -10,6 +12,11 @@ const functions = {
     user["lastName"] = "Santo"
     return user
   },
+  fetchUser: () =>
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => res.data)
+      .catch((err) => "error"),
 }
 
 module.exports = functions
--- jest-tesing-app/src/functions.test.js
+++ jest-tesing-app/src/functions.test.js
@@ -1,5 +1,12 @@
 const functions = require("./functions")
 
+// Check for truthy or falsy values
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
 test("adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4) // toBe() is a matcher
 })
@@ -30,9 +37,38 @@ test("User should be Barkley Santo object", () => {
   })
 })
 
-// Check for truthy or falsy values
-// toBeNull matches only null
-// toBeUndefined matches only undefined
-// toBeDefined is the opposite of toBeUndefined
-// toBeTruthy matches anything that an if statement treats as true
-// toBeFalsy matches anything that an if statement treats as false
+// toBeLessThan and toBeLessThanOrEqual
+
+test("Should be under 1600", () => {
+  const load1 = 800
+  const load2 = 700
+  expect(load1 + load2).toBeLessThan(1600)
+})
+
+// can test regex as well
+
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/) // remember to use /I/ instead of /i/
+})
+
+// Arrays
+test("Admin should be in usernames", () => {
+  const usernames = ["john", "karen", "admin"]
+  expect(usernames).toContain("admin")
+})
+
+// working with async data/ axios
+// test("User fetched name should be Leanne Graham", () => {
+//   expect.assertions(1) // this is needed when using async await
+//   return functions.fetchUser().then((data) => {
+//     expect(data.name).toEqual("Leanne Graham")
+//   })
+// })
+
+
+// Async await
+test("User fetched name should be Leanne Graham", async () => { 
+  expect.assertions(1) // this is needed when using async await
+  const data = await functions.fetchUser()
+  expect(data.name).toEqual("Leanne Graham")
+})
